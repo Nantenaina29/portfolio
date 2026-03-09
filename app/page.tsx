876ec6433387a398a60049452613b1d24269a5ea
Nantenaina29 <elhadari04@gmail.com>
2026-03-09 10:37:46 +0300

    Fix : Update page

--- app/page.tsx
+++ app/page.tsx
@@ -192,11 +192,11 @@ export default function Portfolio() {
                   <div className="p-4 bg-emerald-500/20 rounded-2xl text-emerald-400">
                     <Target size={36} />
                   </div>
-                  <h3 className="text-3xl md:text-4xl font-black  tracking-tighter text-white">ONG TSINJO AINA Fianarantsoa </h3>
+                  <h3 className="text-2xl md:text-4xl font-black  tracking-tighter text-white">ONG TSINJO AINA Fianarantsoa </h3>
                </div>
-               <span className="hidden md:block text-[11px] font-mono bg-sky-500/20 text-sky-400 px-4 py-2 rounded-full uppercase tracking-widest">
-                  Stage Professionnel L2
-               </span>
+               <span className="text-[10px] md:text-[11px] font-mono bg-sky-500/20 text-sky-400 px-3 py-1.5 md:px-4 md:py-2 rounded-full uppercase tracking-widest whitespace-nowrap">
+                Stage Professionnel L2
+              </span>
             </div>
             
             <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
